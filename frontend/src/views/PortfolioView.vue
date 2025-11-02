2cd261617316ae2f4d36f352b64a2596d4de24ac
MMrezaei313 <mr3759959@gmail.com>
2025-11-02 14:43:15 +0330

    Create PortfolioView.vue

---
+++ frontend/src/views/PortfolioView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="portfolio-view">
+    <PortfolioOverview />
+  </div>
+</template>
+
+<script>
+import PortfolioOverview from '@/components/portfolio/PortfolioOverview.vue'
+
+export default {
+  name: 'PortfolioView',
+  components: {
+    PortfolioOverview
+  }
+}
+</script>
+
+<style scoped>
+.portfolio-view {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
