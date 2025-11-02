fb01de4ee70d4565db2bb1e736515b1e6a2b7c0b
MMrezaei313 <mr3759959@gmail.com>
2025-11-02 14:41:59 +0330

    Create DashboardView.vue

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dashboard-view">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from '@/components/dashboard/Dashboard.vue'
+
+export default {
+  name: 'DashboardView',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style scoped>
+.dashboard-view {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
