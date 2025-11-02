01bda7da004fbd73b8e541bc6411f1aa922e272a
MMrezaei313 <mr3759959@gmail.com>
2025-11-02 14:42:24 +0330

    Create TradingView.vue

---
+++ frontend/src/views/TradingView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="trading-view">
+    <LiveTrading />
+  </div>
+</template>
+
+<script>
+import LiveTrading from '@/components/trading/LiveTrading.vue'
+
+export default {
+  name: 'TradingView',
+  components: {
+    LiveTrading
+  }
+}
+</script>
+
+<style scoped>
+.trading-view {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
